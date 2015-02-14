d88c2baeb7e462134918ff943d3e33f345b1a2de
jackmadden87 <jackmadden87@gmail.com>
2015-02-14 00:08:09 +0000

    Still reworking requests, packing seems to be working but not sure. May need to test on a different machine. Still to work out how to insert datawrite into the packed data.

--- req.py
+++ req.py
@@ -14,8 +14,24 @@ class RequestGenerator(object):
         self.length_read = length_read
 
     @staticmethod
+    def print_request_format():
+        """print out a set of strings to show how to build a request"""
+        print "Could not pack this request"
+        print "Request must be a dict of format"
+        print "KEY\t\t\t\tBYTES"
+        print "'command':\t\t\t1"
+        print "'offset_write':\t\t2"
+        print "'length_write':\t\t2"
+        print "'data_write'\t\tn"
+        print "'offset_read:'\t\t2"
+        print "'length_read':\t\t2"
+
+    @staticmethod
     def pack(request):
-        """HbHHnHH"""
+        """Calculate frame length and pack up a request
+           ready for sending to the egate.
+           Method expects Dict with format as defined in
+           RequestGenerator.print_request_format()"""
         try:
             command = request.get('command')
             offset_write = request.get('offset_write')
@@ -24,21 +40,25 @@ class RequestGenerator(object):
             offset_read = request.get('offset_read')
             length_read = request.get('length_read')
 
-            print(command, offset_write, length_write, data_write, offset_read, length_read)
+            # get length of request
+            if data_write is None:
+                frame_length = 9
+            else:
+                frame_length = 9 + len(data_write)
+
+            packing_format = ["H", "b", "H", "H", "H", "H"]
+            data_to_pack = [frame_length, command, offset_write, length_write, offset_read, length_read]
+            packed_request = ""
+
+            for fmt, data in zip(packing_format, data_to_pack):
+                packed_request += struct.pack(">" + fmt, data)
+
+            print struct.unpack(">HbHHHH", packed_request)
 
         except AttributeError:
-            print "Could not pack this request"
-            print "Request must be a dict of format"
-            print "KEY\t\t\t\tBYTES"
-            print "'command':\t\t\t1"
-            print "'offset_write':\t\t2"
-            print "'length_write':\t\t2"
-            print "'data_write'\t\tn"
-            print "'offset_read:'\t\t2"
-            print "'length_read':\t\t2"
-        except struct.error:
-            print "you fucked up!"
-
+            RequestGenerator.print_request_format()
+        #except ValueError:
+        #    RequestGenerator.print_request_format()
 
     def state_request(self, command=1, length_read=65535):
         """Returns a dict containing the request params for a state
@@ -52,8 +72,7 @@ class RequestGenerator(object):
                    "length_read": length_read}
         return request
 
-
 if __name__ == '__main__':
 
     r = RequestGenerator()
-    r.pack("asdfaf")
+    r.pack(r.state_request())

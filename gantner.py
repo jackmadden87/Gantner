078126372837bff62ba9ccc46fc6250734ca5368
jackmadden87 <jackmadden87@gmail.com>
2015-02-15 20:18:07 +0000

    merged req.py into gantner.py. Added placeholders for all request methods. spellinz

--- gantner.py
+++ gantner.py
@@ -52,66 +52,132 @@ class Session(object):
         pass
 
 
-class Request(object):
+class RequestGenerator(object):
 
     def __init__(self, command=0, offset_write=0, length_write=0, data_write=None, offset_read=0, length_read=0):
-        """Initialisation of Dummy request"""
-        self.frameLength = [self.get_frame_length(), 'H']    # 2 byte int
-        self.command = [command, 'b']                        # 1 Byte Int
-        self.offsetWrite = [offset_write, 'H']               # 2 Byte Int
-        self.lengthWrite = [length_write, 'H']               # 2 Byte Int
-        self.dataWrite = data_write                          # n Bytes
-        self.offsetRead = [offset_read, 'H']                 # 2 Byte Int
-        self.lengthRead = [length_read, 'H']                 # 2 Byte Int
+        """init"""
+        self.command = command
+        self.offset_write = offset_write
+        self.length_write = length_write
+        self.data_write = data_write
+        self.offset_read = offset_read
+        self.length_read = length_read
 
-    def get_frame_length(self):
-        """Returns the length of the frame(minus this component), taking into account the length
-           of the data to be sent"""
-        if self.dataWrite is None:
-            return 9
-        else:
-            return 9 + len(self.dataWrite)
+    @staticmethod
+    def print_request_format():
+        """print out a set of strings to show how to build a request"""
+        print "Could not pack this request"
+        print "Request must be a dict of format"
+        print "KEY\t\t\t\tBYTES"
+        print "'command':\t\t\t1"
+        print "'offset_write':\t\t2"
+        print "'length_write':\t\t2"
+        print "'data_write'\t\tn"
+        print "'offset_read:'\t\t2"
+        print "'length_read':\t\t2"
 
-    def pack_frame(self):
-        """Pack the request into hex data ready for transmission"""
-        request = ""
+    @staticmethod
+    def pack(request):
+        """Calculate frame length and pack up a request
+           ready for sending to the egate.
+           Method expects Dict with format as defined in
+           RequestGenerator.print_request_format()"""
+        try:
+            command = request.get('command')
+            offset_write = request.get('offset_write')
+            length_write = request.get('length_write')
+            data_write = request.get('data_write')
+            offset_read = request.get('offset_read')
+            length_read = request.get('length_read')
 
-        for data in [self.frameLength, self.command, self.offsetWrite,
-                     self.lengthWrite, self.dataWrite, self.offsetRead, self.lengthRead]:
-            if data is not None:
-                request += struct.pack(">" + data[1], data[0])
+            # get length of the data to head the request
+            if data_write is None:
+                frame_length = 9
+            else:
+                frame_length = 9 + len(data_write)
 
+            packing_format = ["H", "b", "H", "H", "H", "H"]
+            data_to_pack = [frame_length, command, offset_write, length_write, offset_read, length_read]
+            packed_request = ""
+
+            for fmt, data in zip(packing_format, data_to_pack):
+                packed_request += struct.pack(">" + fmt, data)
+
+            print struct.unpack(">HbHHHH", packed_request)
+            return packed_request
+
+        except AttributeError:
+            RequestGenerator.print_request_format()
+        except ValueError:
+            RequestGenerator.print_request_format()
+
+    def request_variable(self):
+        pass
+
+    def request_state(self, command=1, length_read=65535):
+        """Returns a dict containing the request parameters for a State
+        Request from an egate"""
+
+        request = {"command": command,
+                   "offset_write": self.offset_write,
+                   "length_write": self.length_write,
+                   "data_write": self.data_write,
+                   "offset_read": self.offset_read,
+                   "length_read": length_read}
         return request
 
+    def request_clock(self, command=2, clk=None):
+        """Return a dict containing the request parameters for a Clock
+           Request from an egate. Method defaults to 'get'. To 'set'
+           the clock, pass parameters to 'clk' variable"""
 
-class StateRequest(Request):
-    """Send request for States of machine"""
-    def __init__(self):
-        super(StateRequest, self).__init__(command=1, length_read=65535)
+        if clk is None:
+            # Build a request to GET the Real Time Clock.
 
+            request = {"command": command,
+                       "offset_write": self.offset_write,
+                       "length_write": 0,
+                       "data_write": self.data_write,
+                       "offset_read": self.offset_read,
+                       "length_read": 65535}
 
-class ClockRequest(Request):
-    """Read and Write the RealTimeClock"""
-    def __init__(self, cmd="read", time="None"):
-        self.cmd = cmd
-        self.time = time
-        super(ClockRequest, self).__init__(command=2, length_read=self.read_write())
-
-    def read_write(self):
-        if self.cmd == "read":
-            return 0
-        elif self.cmd == "write":
-            return 9
+            return request
         else:
-            print "Unknown RealTimeClock command, using 'Read' mode"
-            return 0  # todo Handle this better
+            # Build a request to SET the Real Time Clock
 
-    def gen_clock(self):
+            request = {"command": command,
+                       "offset_write": self.offset_write,
+                       "length_write": 9,
+                       "data_write": clk,
+                       "offset_read": self.offset_read,
+                       "length_read": 0}
+
+            return request
+
+    def request_circlebuffer(self):
+        pass
+
+    def request_filetransfer(self):
+        pass
+
+    def request_diagnostics(self, command=9):
+        """Return a dict containing the request parameters for a Diagnostic
+           Request from an egate"""
+        request = {"command": command,
+                   "offset_write": self.offset_write,
+                   "length_write": self.length_write,
+                   "data_write": self.data_write,
+                   "offset_read": self.offset_read,
+                   "length_read": 65535}
+        return request
+
+    def request_logger(self):
         pass
 
 
 if __name__ == '__main__':
-    rqst = StateRequest()
-    e = Egate()
-    e.tcp_connect()
-    e.session.send_request(rqst.pack_frame())
+
+    r = RequestGenerator()
+    r.pack(r.request_state())
+    r.pack(r.request_diagnostics())
+    r.pack(r.request_clock())
